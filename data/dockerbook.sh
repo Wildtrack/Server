93297d6b2bd4b4b38ce6c430b5a46d48c24927ed
Matthew <meneal@gmail.com>
2015-02-07 21:44:19 -0500

    Added dockerbook scripts and adjusted playbook

---
+++ data/dockerbook.sh
@@ -0,0 +1,2 @@
+sudo docker pull meneal/buildbox
+sudo docker run -v /home/vagrant/data:/vol meneal/buildbox sh -c /vol/dockerbook2.sh
---
+++ data/dockerbook2.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/Wildtrack/MiniProject1
+cd MiniProject1
+npm install -g grunt-cli 
+npm install -g bower 
+npm install 
+bower --allow-root install 
+grunt
