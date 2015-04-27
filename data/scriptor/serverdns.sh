6ba531750f8fdf7e0af4aa20f04857ac64c1ec65
Matthew Neal <meneal@gmail.com>
2015-04-26 21:06:25 -0400

    Added provisioning for the DO based server

---
+++ data/scriptor/serverdns.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#Dynamic DNS script from http://kevo.io/code/2012/11/07/cloudflare-dynamic-dns/
+WAN_IP=`wget -O - -q http://ifconfig.me/ip`
+OLD_WAN_IP=`cat /var/CURRENT_WAN_IP.txt`
+if [ "$WAN_IP" = "$OLD_WAN_IP" ]
+then
+        echo "IP Unchanged"
+else
+        curl https://www.cloudflare.com/api_json.html \
+          -d a=rec_edit \
+          -d tkn=$CLOUD_FLARE \
+          -d email=meneal@gmail.com \
+          -d z=lodr.me \
+          -d id=273412012 \
+          -d type=A \
+          -d name=server \
+          -d ttl=1 \
+          -d "content=$WAN_IP"
+        echo $WAN_IP > /var/CURRENT_WAN_IP.txt
+fi
